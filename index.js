a4d25bced31b7d371c6b902596bb9540bda8753d
MinFTW <kimmins2@outlook.com>
2022-06-04 23:29:01 -0500

    heroku fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Setting up express
-const { PORT = 3000 } = process.env;
+const { PORT = 3000 } = process.env
 const express = require('express');
 const server = express();
 
